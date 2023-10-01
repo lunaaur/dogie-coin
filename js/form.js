f6082a94d631e31a01ffc4c8a8169557fcc99cf0
Daria <deimoses04@gmal.com>
2023-10-01 15:51:27 +0300

    final

---
+++ js/form.js
@@ -0,0 +1,43 @@
+let headerBtn = document.getElementById('header-btn')
+let mainBtn = document.getElementById('main-btn')
+let popup = document.getElementById('popup')
+
+let popupBtn = document.getElementById('popup-btn')
+
+popupBtn.addEventListener('click', () => {
+  popup.classList.toggle('open-popup')
+})
+
+headerBtn.addEventListener("click", () => {
+
+  popup.classList.toggle('open-popup')
+  document.querySelector('body').classList.toggle('lock');
+
+}
+);
+
+document.getElementById('main-btn').addEventListener("click", function(event) {
+  document.querySelector('body').classList.toggle('lock');
+
+  popup.classList.toggle('open-popup')
+}
+);
+
+
+const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+
+const input = document.getElementById('popup__input');
+
+function onInput() {
+  if (isEmailValid(input.value)) {
+    input.style.borderColor = 'green';
+  } else {
+    input.style.borderColor = 'red';
+  }
+}
+
+input.addEventListener('input', onInput);
+
+function isEmailValid(value) {
+return EMAIL_REGEXP.test(value);
+}
